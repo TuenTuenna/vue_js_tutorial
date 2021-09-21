81a8a08b8317afa7ba13693d9c27f36e9f08f5d9
Jeff, Jeong  정 의정 <35421421+TuenTuenna@users.noreply.github.com>
2021-09-22 00:33:13 +0900

    뷰cli 설치 및 뷰 개념 맛보기

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,38 @@
+<!-- html 요소 -->
+<template>
+    <h1>타이틀: {{ title }} 입니다.</h1>
+    <h1>count: {{ count }}</h1>
+    <button @click="sayHello">인사하세요~</button>
+    <button @click="countUp">카운트 업</button>
+</template>
+
+<!-- 스크립트 - 로직 && 데이터 변수   -->
+<script>
+export default {
+    name: 'MyComponent',
+    // 사용하는 데이터 변수 
+    data(){
+        return {
+            title: '쩡대리',
+            count: 0
+        }
+    },
+    // 사용하는 메소드 들 정의 
+    methods: {
+        sayHello(){
+            this.title = "안녕하세요?";
+        },
+        countUp(){
+            this.count += 1;
+        }
+    }
+}
+</script>
+
+<!-- 스타일링  -->
+<style scoped>
+    h1 {
+        background: green;
+        color: white;
+    }
+</style>
